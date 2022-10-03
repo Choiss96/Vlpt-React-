9aa27902dd7636b3bcce9b3c65cd8e999643957e
Miro <asc9602@naver.com>
2022-10-03 18:27:20 +0900

    Hello.js 수정

--- vlpt-react-practice/src/Hello.js
+++ vlpt-react-practice/src/Hello.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 function Hello({ color, name }) {
-  return (
-    <>
-      <div style={{ color }}>안녕하세요 {name}</div>
-      <div>테스트 내용 추가</div>
-    </>
-  )
+  return <div style={{ color }}>안녕하세요 {name}</div>
+}
+
+Hello.defaultProps = {
+  name: '이름없음'
 }
 
 export default Hello;
