e42800e1ebab2968ca1055308cd73638c5144dc4
Miro <asc9602@naver.com>
2022-10-19 23:24:52 +0900

    useState 함수형 추가

--- vlpt-react-practice/src/Counter.js
+++ vlpt-react-practice/src/Counter.js
@@ -4,11 +4,11 @@ function Counter() {
     const [number, setNumber] = useState(0);
 
     const onIncrease = () => {
-        setNumber(number + 1);
+        setNumber(number => number + 1);
     }
 
     const onDecrease = () => {
-        setNumber(number - 1);
+        setNumber(number => number - 1);
     }
 
     return (
