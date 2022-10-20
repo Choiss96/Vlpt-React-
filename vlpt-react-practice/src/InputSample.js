92c2f9f647f28c343bb98c79905414d016dc82fe
Miro <asc9602@naver.com>
2022-10-20 12:34:48 +0900

    여러개 input 상태 관리

--- vlpt-react-practice/src/InputSample.js
+++ vlpt-react-practice/src/InputSample.js
@@ -1,25 +1,40 @@
 import React, { useState } from 'react';
 
 function InputSample() {
-  const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: '',
+    });
 
-  const onChange = (e) => {
-    setText(e.target.value);
-  };
+    const { name, nickname } = inputs;
 
-  const onReset = () => {
-    setText('');
-  };
+    const onChange = (e) => {
+        const { value, name } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
 
-  return (
-    <div>
-      <input onChange={onChange} value={text}  />
-      <button onClick={onReset}>초기화</button>
-      <div>
-        <b>값: {text}</b>
-      </div>
-    </div>
-  );
-}
-
-export default InputSample;
+    const onReset = () => {
+        setInputs({
+          name: '',
+          nickname: '',
+        })
+      };
+    
+    
+      return (
+        <div>
+          <input name="name" placeholder="이름" onChange={onChange} value={name} />
+          <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
+          <button onClick={onReset}>초기화</button>
+          <div>
+            <b>값: </b>
+            {name} ({nickname})
+          </div>
+        </div>
+      );
+    }
+    
+    export default InputSample;
