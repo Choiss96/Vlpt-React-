58f41705412d77b38bec0722b59cda6b99a3b7db
Miro <asc9602@naver.com>
2022-10-22 00:01:30 +0900

    useref 연습준비

--- vlpt-react-practice/src/App.js
+++ vlpt-react-practice/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
-import Counter from './Counter';
-import InputSample from './InputSample';
+
+import UserList from './UserList';
 
 function App() {
-  return (
-    <InputSample />
-  )
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+  return <UserList users={users} />;
 }
 
 export default App;
